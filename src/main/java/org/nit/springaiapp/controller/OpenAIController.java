772b664747c3362a10c7c19bb2deee66da0a6c38
Naveen Prasanna <naveenprasannavnp@gmail.com>
2026-03-13 19:57:23 +0530

    PG Vector added

--- src/main/java/org/nit/springaiapp/controller/OpenAIController.java
+++ src/main/java/org/nit/springaiapp/controller/OpenAIController.java
@@ -2,15 +2,31 @@ package org.nit.springaiapp.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class OpenAIController {
     private ChatClient chatClient;
+
+    @Autowired
+    @Qualifier("openAiEmbeddingModel")
+    private EmbeddingModel embeddingModel;
+
+    @Autowired
+    private VectorStore vectorStore;
 
 
     public OpenAIController(OpenAiChatModel chatModel) {
@@ -41,4 +57,56 @@ public class OpenAIController {
                 .getText();
         return ResponseEntity.ok(response);
     }
+
+    @PostMapping("/api/recommend")
+    public String recommend(@RequestParam String type, @RequestParam String year, @RequestParam String lang) {
+
+        String tempt = """
+                I want to watch a good rated {type} movie released in the year {year} in {lang} language.
+                Make sure it is top rated movie.
+                Suggest me one specific movie with cast and running time.
+                """;
+
+        PromptTemplate promptTemplate = new PromptTemplate(tempt);
+        Prompt prompt = promptTemplate.create(Map.of("type", type, "year", year, "lang", lang));
+
+        String response = chatClient
+                .prompt(prompt)
+                .call()
+                .content();
+
+        return response;
+    }
+
+    @PostMapping("/api/embedding")
+    public float[] embedding(@RequestParam String text) {
+        return embeddingModel.embed(text);
+    }
+
+    @PostMapping("/api/similarity")
+    public double getSimilarity(@RequestParam String text1, @RequestParam String text2) {
+
+        float[] embedding1 = embeddingModel.embed(text1);
+        float[] embedding2 = embeddingModel.embed(text2);
+
+        double dotProduct = 0;
+        double norm1 = 0;
+        double norm2 = 0;
+
+        for (int i = 0; i < embedding1.length; i++) {
+            dotProduct += embedding1[i] * embedding2[i];
+            norm1 += Math.pow(embedding1[i], 2);
+            norm2 += Math.pow(embedding2[i], 2);
+        }
+
+        return dotProduct * 100 / (Math.sqrt(norm1) * Math.sqrt(norm2));
+    }
+
+    @PostMapping("/api/product")
+    public List<Document> getProducts(@RequestParam String text) {
+         // return vectorStore.similaritySearch(text);
+        return vectorStore.similaritySearch(SearchRequest.builder().query(text).topK(2).build());
+    }
+
+
 }
